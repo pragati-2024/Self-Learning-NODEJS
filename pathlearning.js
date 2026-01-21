5b5a00cee0697eee31c9306f92c6615fa06aac95
pragati-2024 <bpragati21@gmail.com>
2026-01-21 18:43:12 +0530

    learning paths

---
+++ pathlearning.js
@@ -0,0 +1,7 @@
+// path module is built in core module that provide utilities for working with file and directory paths
+// global constant is a value that is always available in your code without requiring an import or declaration
+
+const path = require('path');
+const file ="./text/reshu.txt"
+console.log(path.extname(file));
+
